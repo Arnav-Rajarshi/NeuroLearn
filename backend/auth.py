ae609f2b60e984b2b9791fc4cb86f07b18e40868
Arnav_Rajarshi <arnavrajarshi@gmail.com>
2026-03-22 20:16:13 +0530

    debugging

--- backend/auth.py
+++ backend/auth.py
@@ -52,12 +52,11 @@ class LoginRequest(BaseModel):
 
 
 # Helper Functions
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password[:72], hashed_password)
 
-
-def get_password_hash(password: str) -> str:
-    return pwd_context.hash(password)
+def get_password_hash(password):
+    return pwd_context.hash(password[:72])
 
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
