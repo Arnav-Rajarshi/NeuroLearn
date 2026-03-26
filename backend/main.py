c644abe69eb0020587e4c3b704229027622fc3e7
Arnav_Rajarshi <arnavrajarshi@gmail.com>
2026-03-27 00:38:44 +0530

    wowowow2

--- backend/main.py
+++ backend/main.py
@@ -28,9 +28,11 @@ origins = [
     "http://127.0.0.1:5173",
 ]
 
+allow_origins=["*"]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
